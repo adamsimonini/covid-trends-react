934732573fbbafb84827492574bbd2a07c131f7a
Adam Simonini <adam.simonini@gmail.com>
2021-12-13 11:56:14 -0500

    another try

--- models/index.js
+++ models/index.js
@@ -4,14 +4,14 @@ const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-// const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js");
 const db = {};
 
 let sequelize;
-
-if (process.env.NODE_ENV == "production") {
-	sequelize = new Sequelize(config.use_env_variable, null, null, { dialect: config.dialect });
+console.log(config);
+if (config.use_env_variable) {
+	sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
 	sequelize = new Sequelize(config.database, config.username, config.password, config.connection, config);
 }
